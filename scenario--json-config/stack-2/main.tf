44e99a74ed90afe38bef2f94c2f71b807c3be529
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-03-16 22:33:11 +1100

    Map function in terraform config!

--- scenario--json-config/stack-2/main.tf
+++ scenario--json-config/stack-2/main.tf
@@ -1,6 +1,9 @@
 variable "variable_2" {}
 variable "st1_variable_1" {}
 variable "common_variable_2" {}
+variable "common_list" {
+  type = list(string)
+}
 
 output "out_variable_2" {
   value = "Here is ${var.variable_2}: ${var.st1_variable_1}"
@@ -8,4 +11,8 @@ output "out_variable_2" {
 
 output "out_common_variable_2" {
   value = "Here is ${var.common_variable_2}"
+}
+
+output "out_common_list" {
+  value = [for item in var.common_list: "${item}!" ]
 }
